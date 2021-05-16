469383d6a5c0c59cc5b00467b1473e1edf05cf40
Mateus Pinho <mateusleitepinho@gmail.com>
2021-05-16 11:43:52 +0100

    projeto comecado

--- proj.c
+++ proj.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include "funcoes.h"
 
 /* Defines */
 
 #define MAX_CHAR 65535
 
-int main () 
+/* string defines */
+#define HELP "help"
+#define QUIT "quit"
+
+int main() 
 {   
     /*Variavel que contem o comando a ser executado*/
     char input[MAX_CHAR + 1];
     /*Le carateres da stdin ate encontrar o caso terminal ou 
     um comando a executar*/
     do {
-        fgets(input, (MAX_CHAR + 1), stdin);
-        switch(input) {
-            case "help":
-                help();
-                break;
+        scanf("%s", input);
+        if (strcmp(input, HELP) == 0) {
+            help();
         }
     }
-    while((strcmp(input, "quit") != 0));
-    return 0;    
+    while(strcmp(input, QUIT) != 0);
+    return 0;
 }
